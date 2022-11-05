6fd4817fd84e568a6e14a1bf42c977c5a36f23c6
MichelleVides123 <demaryoryorellana2112@gmail.com>
2022-11-04 18:02:03 -0600

    clases de los proyectos
    segundo cambio

---
+++ EXAMEN PARCIAL II/Entidades/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+    }
+}
