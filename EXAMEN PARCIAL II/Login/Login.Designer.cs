8c586981764335fb827298938a7a4cad5db68d3e
MichelleVides123 <demaryoryorellana2112@gmail.com>
2022-11-04 19:12:14 -0600

    ingresar formulario
    tercer ejecucion

--- EXAMEN PARCIAL II/Login/Login.Designer.cs
+++ EXAMEN PARCIAL II/Login/Login.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aceptarbutton = new System.Windows.Forms.Button();
+            this.cancelarbutton = new System.Windows.Forms.Button();
+            this.usuariotextBox = new System.Windows.Forms.TextBox();
+            this.clavetextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clave: ";
+            // 
+            // aceptarbutton
+            // 
+            this.aceptarbutton.Location = new System.Drawing.Point(48, 120);
+            this.aceptarbutton.Name = "aceptarbutton";
+            this.aceptarbutton.Size = new System.Drawing.Size(79, 38);
+            this.aceptarbutton.TabIndex = 2;
+            this.aceptarbutton.Text = "Aceptar";
+            this.aceptarbutton.UseVisualStyleBackColor = true;
+            this.aceptarbutton.Click += new System.EventHandler(this.aceptarbutton_Click);
+            // 
+            // cancelarbutton
+            // 
+            this.cancelarbutton.Location = new System.Drawing.Point(164, 120);
+            this.cancelarbutton.Name = "cancelarbutton";
+            this.cancelarbutton.Size = new System.Drawing.Size(82, 38);
+            this.cancelarbutton.TabIndex = 3;
+            this.cancelarbutton.Text = "Cancelar";
+            this.cancelarbutton.UseVisualStyleBackColor = true;
+            this.cancelarbutton.Click += new System.EventHandler(this.cancelarbutton_Click);
+            // 
+            // usuariotextBox
+            // 
+            this.usuariotextBox.Location = new System.Drawing.Point(98, 33);
+            this.usuariotextBox.Name = "usuariotextBox";
+            this.usuariotextBox.Size = new System.Drawing.Size(148, 23);
+            this.usuariotextBox.TabIndex = 4;
+            // 
+            // clavetextBox
+            // 
+            this.clavetextBox.Location = new System.Drawing.Point(98, 73);
+            this.clavetextBox.Name = "clavetextBox";
+            this.clavetextBox.Size = new System.Drawing.Size(148, 23);
+            this.clavetextBox.TabIndex = 5;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 314);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(327, 191);
+            this.Controls.Add(this.clavetextBox);
+            this.Controls.Add(this.usuariotextBox);
+            this.Controls.Add(this.cancelarbutton);
+            this.Controls.Add(this.aceptarbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button aceptarbutton;
+        private Button cancelarbutton;
+        private TextBox usuariotextBox;
+        private TextBox clavetextBox;
+        private ErrorProvider errorProvider1;
     }
 }
--- EXAMEN PARCIAL II/Login/Login.cs
+++ EXAMEN PARCIAL II/Login/Login.cs
@@ -1,3 +1,5 @@
+using Datos;
+
 namespace Login
 {
     public partial class Login : Form
@@ -5,6 +7,43 @@ namespace Login
         public Login()
         {
             InitializeComponent();
+        }
+
+        private async void aceptarbutton_Click(object sender, EventArgs e)
+        {
+            if (usuariotextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(usuariotextBox, "Ingrese un codigo de usuario");
+                usuariotextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (clavetextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(clavetextBox, "Ingrese una clave");
+                clavetextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            UsuarioDatos userDatos = new UsuarioDatos();
+            bool valido = await userDatos.LoginAsync(usuariotextBox.Text, clavetextBox.Text);
+
+            if (valido)
+            {
+                Menu formulario = new Menu();
+                Hide();
+                formulario.Show();
+            }
+            else
+            {
+                MessageBox.Show("Datos incorrectos", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cancelarbutton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
